f928abcb950c4afcbd669627b7766b7ca54a67b9
Aur71 <111057774+Aur71@users.noreply.github.com>
2022-08-12 16:08:29 +0300

    JS update

--- app.js
+++ app.js
@@ -9,60 +9,20 @@ submitBtn.onclick = () => {
 };
 
 // Rating below
-const btn1 = document.getElementById("btn1");
-const btn2 = document.getElementById("btn2");
-const btn3 = document.getElementById("btn3");
-const btn4 = document.getElementById("btn4");
-const btn5 = document.getElementById("btn5");
-const allBtns = document.querySelectorAll(".ratingBtn");
+
 const span = document.getElementsByTagName("span")[0];
+const ratingBtn = document.querySelectorAll(".ratingBtn");
 
-btn1.onclick = () => {
-  span.innerText = 1;
-  btn1.classList.add("active");
-  btn2.classList.remove("active");
-  btn3.classList.remove("active");
-  btn4.classList.remove("active");
-  btn5.classList.remove("active");
-  submitBtn.disabled = false;
-};
-
-btn2.onclick = () => {
-  span.innerText = 2;
-  btn2.classList.add("active");
-  btn1.classList.remove("active");
-  btn3.classList.remove("active");
-  btn5.classList.remove("active");
-  btn5.classList.remove("active");
-  submitBtn.disabled = false;
-};
-
-btn3.onclick = () => {
-  span.innerText = 3;
-  btn3.classList.add("active");
-  btn1.classList.remove("active");
-  btn2.classList.remove("active");
-  btn4.classList.remove("active");
-  btn5.classList.remove("active");
-  submitBtn.disabled = false;
-};
-
-btn4.onclick = () => {
-  span.innerText = 4;
-  btn4.classList.add("active");
-  btn1.classList.remove("active");
-  btn2.classList.remove("active");
-  btn3.classList.remove("active");
-  btn5.classList.remove("active");
-  submitBtn.disabled = false;
-};
-
-btn5.onclick = () => {
-  span.innerText = 5;
-  btn5.classList.add("active");
-  btn1.classList.remove("active");
-  btn2.classList.remove("active");
-  btn3.classList.remove("active");
-  btn4.classList.remove("active");
-  submitBtn.disabled = false;
-};
+for(let i = 0; i < ratingBtn.length; i++) {
+  ratingBtn[i].addEventListener('click', (e) => {
+    submitBtn.disabled = false;
+    ratingBtn[0].classList.remove("active");
+    ratingBtn[1].classList.remove("active");
+    ratingBtn[2].classList.remove("active");
+    ratingBtn[3].classList.remove("active");
+    ratingBtn[4].classList.remove("active");
+    ratingBtn[i].classList.add("active")
+    const spanValue = i + 1;
+    span.innerText = spanValue
+  })
+}
